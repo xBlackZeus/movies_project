44d916de0f3ff094e72b4c2f679ee0275279fe87
Mohammad Fathi <mohfabdou@gmail.com>
2024-07-20 01:01:44 +0300

    front end setup + Added simple navbar with website logo

---
+++ frontend/untitled_movies_project/src/App.jsx
@@ -0,0 +1,25 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='nav'>
+      <div className='navbarContainer'>
+        <img id='logo' src="logoLight.png" alt="" />
+        <ul className='navbar'>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Browse</a></li>
+          <li><a href="#">Trending</a></li>
+          <li><a href="#">About Us</a></li>
+        </ul>
+      </div>
+    </div>
+      <div></div>
+    </>
+  )
+}
+
+export default App
